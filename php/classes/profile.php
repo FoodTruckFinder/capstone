0b761e9f3f542801c4e0b2211b56e1a6c5f9e64d
Daniel Nakitare <dnakitare@cnm.edu>
2018-11-05 16:05:17 -0700

    8180 create class 'profile' and added private statements

--- php/classes/profile.php
+++ php/classes/profile.php
@@ -1,1 +1,37 @@
 <?php
+
+class profile implements \JsonSerializable {
+
+	use ValidateUuid;
+
+	/**
+	 * id for this profile; this is a primary key
+	 * @var Uuid $profileId
+	 */
+	private $profileId;
+	/**
+	 * activation token for this profile
+	 * @var string $profileActivationToken
+	 */
+	private $profileActivationToken;
+	/**
+	 * the email associated with this profile
+	 * @var string $profileEmail
+	 */
+	private $profileEmail;
+	/**
+	 * the hashed password of this profile
+	 * @var string $profileHash
+	 */
+	private $profileHash;
+	/**
+	 * the boolean to check if profile is a truck owner
+	 * @var integer $profileIsOwner
+	 */
+	private $profileIsOwner;
+	/**
+	 * the profile name provided by the user
+	 * @var string $profileName
+	 */
+	private $profileName;
+}
