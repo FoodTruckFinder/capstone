55ceee099b1b8f41f1524cc134bed7fcee7f8c93
dsanderson90 <dsanderson3@cnm.edu>
2018-11-05 15:18:53 -0700

    added Georges changes

--- epic/foodtrucktable.sql
+++ epic/foodtrucktable.sql
@@ -12,8 +12,8 @@ CREATE TABLE profile (
 	profileId BINARY(16) NOT NULL,
 	profileActivationToken CHAR(32),
 	profileEmail VARCHAR(128) NOT NULL,
-	profileHash VARCHAR(96) NOT NULL,
-	profileIsOwner BIT DEFAULT 0 NOT NULL,
+	profileHash VARCHAR(97) NOT NULL,
+	profileIsOwner TINYINT NOT NULL,
 	profileName VARCHAR(32) NOT NULL,
 
 	UNIQUE(profileName),
@@ -27,13 +27,12 @@ CREATE TABLE foodTruck (
 	foodTruckId BINARY (16) NOT NULL,
 	foodTruckProfileId BINARY (16) NOT NULL,
 	foodTruckDescription VARCHAR (32) NULL,
-	foodTruckImageUrl VARCHAR (128) NOT NULL,
+	foodTruckImageUrl VARCHAR (255) NOT NULL,
 	foodTruckMenuUrl VARCHAR (255) NULL,
 	foodTruckName VARCHAR (32) NOT NULL,
-	foodTruckPhoneNumber BIGINT (150) NULL,
+	foodTruckPhoneNumber STRING (16) NULL,
 
 	UNIQUE(foodTruckName),
-	UNIQUE(foodTruckId),
 	UNIQUE(foodTruckProfileId),
 	INDEX (foodTruckProfileId),
 	FOREIGN KEY(foodTruckProfileId) REFERENCES Profile(ProfileId),
@@ -58,10 +57,9 @@ CREATE TABLE social (
 
 	socialId BINARY(16) NOT NULL,
 	socialFoodTruckId BINARY(16) NOT NULL,
-	socialUrrl VARCHAR(255) NOT NULL,
+	socialUrl VARCHAR(255) NOT NULL,
 	INDEX(socialFoodTruckId),
 
-	UNIQUE(socialFoodTruckId),
 	PRIMARY KEY(socialId),
 	FOREIGN KEY (socialFoodTruckId) REFERENCES foodTruck(foodTruckId)
 
@@ -71,10 +69,10 @@ CREATE TABLE location (
 
 	locationId BINARY(16) NOT NULL,
 	locationFoodtruckId BINARY(16) NOT NULL,
-	locationEnd DATETIME NOT NULL,
+	locationEndTime DATETIME NOT NULL,
 	locationLatitude DECIMAl(9,6) NOT NULL,
 	locationLongitude DECIMAl(9,6) NOT NULL,
-	locationStart DATETIME NOT NULL,
+	locationStartTime DATETIME NOT NULL,
 
 	UNIQUE(locationId),
 	UNIQUE(locationFoodtruckId),
