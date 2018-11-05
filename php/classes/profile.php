bd6e79d9c064a3a06bfc83c29b7ded993570e0c8
Daniel Nakitare <dnakitare@cnm.edu>
2018-11-05 16:21:21 -0700

    8180 added constructor initial docblock and begin writing public constructor function

--- php/classes/profile.php
+++ php/classes/profile.php
@@ -34,4 +34,25 @@ class profile implements \JsonSerializable {
 	 * @var string $profileName
 	 */
 	private $profileName;
+
+	/**
+	 * constructor for this profile
+	 *
+	 * @param string|Uuid $newProfileId id of this profile or null if new profile
+	 * @param string $newProfileActivationToken activation token of this profile or null if new profile
+	 * @param string $newProfileEmail email of this profile or null if new profile
+	 * @param string $newProfileHash hashed password of this profile or null if new profile
+	 * @param integer $newProfileIsOwner boolean assigning truck owner status of this profile or null if new profile
+	 * @param string $newProfileName name of the profile or null if new profile
+	 * @throws \InvalidArgumentException if the data types are not valid
+	 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+	 * @throws \TypeError if data types violate type hints
+	 * @throws \Exception if some other exception occurs
+	 */
+
+	public function construct($newProfileId, $newProfileActivationToken, $newProfileEmail, $newProfileHash, $newProfileIsOwner, $newProfileName) {
+		try {
+			$this->setProfileId($newProfileId);
+		}
+	}
 }
