467c44575ffa7f9fa2d6ecb25e8d06ab92c69da6
Daniel Nakitare <dnakitare@cnm.edu>
2018-11-07 14:42:19 -0700

    finished building private static function validateUuid

--- php/classes/validateUuid.php
+++ php/classes/validateUuid.php
@@ -31,6 +31,28 @@ trait ValidateUuid {
 				$newUuid = substr($newUuid, 0, 8) . "-" . substr($newUuid, 8, 4) . "-" . substr($newUuid,12,4) . "-" . substr($newUuid,16,4) . "-" . substr($newUuid,20,12);
 			}
 			// 36 characters is human readable uuid
+			if(strlen($newUuid) === 36) {
+				if(Uuid::isValid($newUuid) === false) {
+					throw (new \InvalidArgumentException("invalid uuid"));
+					$uuid = Uuid::fromString($newUuid);
+				}
+				else {
+					throw (new \InvalidArgumentException("invalid uuid"));
+				}
+			}
+			elseif(gettype($newUuid) === "object" && get_class($newUuid) === "Ramsey\Uuid\Uuid") {
+				// if the misquote id is already a valid UUID, press on
+				$uuid = $newUuid;
+			}
+			else {
+				//throw out any other trash
+				throw (new \InvalidArgumentException("invalid uuid"));
+			}
+			// verify the uuid is uuid v4
+			if($uuid->getVersion() !== 4) {
+				throw (new \RangeException("uuid is incorrect version"));
+			}
+			return($uuid);
 		}
 	}
 }
