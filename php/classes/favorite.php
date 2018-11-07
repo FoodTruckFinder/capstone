7a6a6f378a1a8eff489213917bcd7b4d9cc07804
Bernina Gray <bernina.gray@icloud.com>
2018-11-07 12:22:26 -0700

    8181 constructor for FavoriteProfileId

--- php/classes/favorite.php
+++ php/classes/favorite.php
@@ -1,6 +1,10 @@
 <?php
 
-namespace Edu/Cnm/$foodFinderFinder;
+namespace Edu/
+
+use http\Exception\InvalidArgumentException;
+
+Cnm/$foodFinderFinder;
 
 require_once "autoload.php";
 require_once (dirname(__DIR__, 2)) . "vendor/autoload.php";
@@ -10,8 +14,8 @@ class Favorite implements \JsonSerializable {
 		use ValidateUuid;
 
 		/**
-		 * id for favoriteUser; this is a foreign key
-		 * @var Uuid $favoriteUserId
+		 * id for favoriteProfile; this is a foreign key
+		 * @var Uuid $favoriteProfileId
 		 */
 		private $favoriteUserId;
 		/**
@@ -26,6 +30,19 @@ class Favorite implements \JsonSerializable {
 
 		/** constructor for this favorite
 		 *
-		 * @param Uuid | string $newFavoriteUserId if of
+		 * @param Uuid | string $newFavoriteProfileId is uuid for profile that favorited the foodtruck
+		 * @param Uuid | string $newFavoriteTruckId is uuid for the favorited truck
+		 * @param DateTime | $newFavoriteAddDate  datetime value
+		 * @throws \InvalidArgumentException if the data types are not valid
+		 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
+		 * @throws \TypeError if data types violate type hints
+		 * @throws \Exception if some other exception occurs
 		 */
+		public function __construct($newFavoriteProfileId, $newFavoriteTruckId, $newFavoriteAddDate) {
+			try {
+				$this->setFavoriteProfileId($newFavoriteProfileId);
+				$this->setFavoriteTruckId($newFavoriteTruckId);
+				$this->set
+			}
+		}
 }
