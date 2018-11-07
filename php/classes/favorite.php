9512eb563224ccd241fe67c59dcaff8dc2d6cc18
Bernina Gray <bernina.gray@icloud.com>
2018-11-07 14:20:41 -0700

    8181 done with favoriteFoodTruckId accessors and mutator methods

--- php/classes/favorite.php
+++ php/classes/favorite.php
@@ -1,6 +1,6 @@
 <?php
 
-namespace Edu/Cnm/$FoodTruckFinder;
+namespace Edu/Cnm/FoodTruckFinder;
 require_once "autoload.php";
 require_once (dirname(__DIR__, 2)) . "vendor/autoload.php";
 
@@ -17,7 +17,7 @@ class Favorite implements \JsonSerializable {
 		 * id for favoriteTruck; this is a foreign key
 		 * @var Uuid $favoriteTruckId
 		 */
-		private $favoriteTruckId;
+		private $favoriteFoodTruckId;
 		/**
 		 * stores the datetime for when a foodtruck is  added as a favorite
 		 */
@@ -26,7 +26,7 @@ class Favorite implements \JsonSerializable {
 		/** constructor for this favorite
 		 *
 		 * @param Uuid | string $newFavoriteProfileId is uuid for profile that favorited the foodtruck
-		 * @param Uuid | string $newFavoriteTruckId is uuid for the favorited truck
+		 * @param Uuid | string $newFavoriteFoodTruckId is uuid for the favorited truck
 		 * @param DateTime | $newFavoriteAddDate  datetime value
 		 * @throws \InvalidArgumentException if the data types are not valid
 		 * @throws \RangeException if data values are out of bounds (e.g., strings too long, negative integers)
@@ -36,7 +36,7 @@ class Favorite implements \JsonSerializable {
 		public function __construct($newFavoriteProfileId, $newFavoriteTruckId, $newFavoriteAddDate) {
 			try {
 				$this->setFavoriteProfileId($newFavoriteProfileId);
-				$this->setFavoriteTruckId($newFavoriteTruckId);
+				$this->setFavoriteFoodTruckId($newFavoriteTruckId);
 				$this->setFavoriteAddDate($newFavoriteAddDate);
 			}
 			catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
@@ -47,6 +47,51 @@ class Favorite implements \JsonSerializable {
 
 		/** accessor method for favorite profile id
 		 *
-		 * @param
+		 * @return Uuid value of the favorite profile id
 		 */
+		public function getFavoriteProfileId() : Uuid {
+			return $this->favoriteProfileId;
+		}
+		/** mutator method for favorite profile id
+		 *
+		 * @param Uuid | string $newFavoriteProfileId new value of the favorite profile id
+		 * @throws \RangeException if $newFavoriteProfileId is not positive
+		 * @throws \TypeError if $newFavoriteProfileId violates type hints
+		 */
+		public function setFavoriteProfileId(uuid $newFavoriteProfileId) : void {
+			// verify the id is a valid uuid
+			try {
+				$uuid = self::validateUuid(newFavoriteProfileId);
+			} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+
+			}
+			// store the uuid
+			$this->favoriteProfileId = $uuid;
+		}
+
+		/** accessor method for favorite food truck id
+		 *
+		 * @return Uuid value of the favorite food truck id
+		 */
+		public function getFavoriteFoodTruckId() : uuid {
+			return $this->favoriteFoodTruckId;
+		}
+		/**
+		 * mutator method for favorite food truck id
+		 *
+		 * @param Uuid | string $newFavoriteFoodTruckId new value of the favorited food truck id
+		 * @throws \RangeException if $newFavoriteFoodTruckId is not positive
+		 * @throws \TypeError if $newFavoriteFoodTruckId violates type hints
+		 */
+		public function setFavoriteFoodTruckId(uuid $newFavoriteFoodTruckId) : void {
+			// verify the id is a valid uuid
+			try {
+				$uuid = self::validateUuid($newFavoriteFoodTruckId);
+			} catch(\InvalidArgumentException | \RangeException | \Exception | \TypeError $exception) {
+
+			}
+			//store the uuid
+			$this->favoriteFoodTruckId = $uuid;
+		}
+
 }
