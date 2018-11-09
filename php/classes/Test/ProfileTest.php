4c9f18abed5ce97e22a90c4cbb6d564050ae37a3
Daniel Nakitare <dnakitare@cnm.edu>
2018-11-09 11:37:42 -0700

    8180 began building class ProfileTest

--- php/classes/Test/ProfileTest.php
+++ php/classes/Test/ProfileTest.php
@@ -1,2 +1,16 @@
 <?php
 
+namespace Edu\Cnm\FoodTruckFinder\Test;
+
+use Edu\Cnm\FoodTruckFinder\Profile;
+
+
+/**
+ * PHPUnit test of the Profile class.
+ *
+ * @see Profile
+ * @author Daniel Nakitare
+ */
+class ProfileTest extends FoodTruckTest {
+
+}
