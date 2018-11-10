2c48cabfc8cc21b9a9c42292c4ae7503e888ead6
Bernina Gray <bernina.gray@icloud.com>
2018-11-10 12:34:31 -0700

    8181 testing valid and invalid cases for each of the classes

--- php/classes/Test/FavoriteTest.php
+++ php/classes/Test/FavoriteTest.php
@@ -17,5 +17,23 @@ require_once(dirname(__DIR__, 2) . "/lib/uuid.php");
  * @author Bernina Gray <bgray11@cnm.edu>
  **/
 class FavoriteTest extends FoodTruckTest {
+		/**
+		 * Profile that created the favorite; this is the foreign key relations
+		 * @var Favorite profile
+		 **/
+		protected $profile = null;
+
+		/**
+		 * the id referencing the favorited food truck
+		 * @var favorite id
+		 **/
+		protected $id = null;
+
+		/**
+		 * timestamp of the Favorite; this starts as null and is assigned later
+		 * @var \DateTime $VALID_TWEETDATE
+		 **/
+		protected $VALID_FAVORITEDATE = null;
+
 
 }
