28afbb580f8b8ad2f533a9deab8a70608516a1c9
Bernina Gray <bernina.gray@icloud.com>
2018-11-11 17:32:41 -0700

    8181 Adding test insert valid favorite to line 93

--- php/classes/Test/FavoriteTest.php
+++ php/classes/Test/FavoriteTest.php
@@ -42,14 +42,36 @@ class FavoriteTest extends FoodTruckTest {
 public final function setUp() : void {
 		//run the default setUp() method first
 		parent::setUp();
+
 		// create a salt and hash for the mocked profile
 		$password = "abc123";
 		$this->VALID_PROFILE_HASH = password_hash($password, PASSWORD_ARGON2I, ["time_cost" => 384]);
 		$this->VALID_ACTIVATION = bin2hex(random_bytes(16));
+
 		// create and insert the mocked profile
-		$this->profile = new Profile(generateUuidV4(), null, "@phpunit";
+		$this->profile = new Profile(generateUuidV4(), null, "@phpunit" "test@phpunit.de", $this->>VALID_HASH, "+12125551212");
 		$this->profile->insert($this->getPDO());
+
 		// create and insert the mocked favorite
 		$this->favorite = new Favorite(generateUuidV4), $this->profile->getProfileId, "PHPUnit like test passing");
+		$this->favorite->insert($this->getPDO());
 
+		// calculate the date (just use the time the unit test was setup...)
+		$this->VALID_LIKEDATE = new \DateTime();
+}
+
+/**
+ * test inserting a valid Favorite and verify that the actual mySQL data matches
+ **/
+public function testInsertValidFavorite() : void {
+		// count the number of rows and save it for later
+		$numRows = $this->getConnection()->getRowCount("Favorite");
+
+		// create a new Favorite and insert to into mySQL
+		$favorite = new Favorite($this->profile->getProfileId(), $this->foodTruck->getFoodTruckId(), $this->VALID_FAVORITEDATE);
+		$favorite->insert($this->getPDO());
+
+		// grab the data from mySQL and enforce the fields match our expectations
+
+	}
 }
