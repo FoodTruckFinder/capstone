8fe9f1a8203eeb7cd48a2d890c1281c65c6d395a
dsanderson90 <dsanderson3@cnm.edu>
2018-11-12 13:33:18 -0700

    8182 - making insert valid location werk

--- php/classes/Test/LocationTest.php
+++ php/classes/Test/LocationTest.php
@@ -99,8 +99,7 @@ class LocationTest extends FoodTruckFinderTest {
 
 		// create a new Tweet and insert to into mySQL
 		$locationId = generateUuidV4();
-		$locationFoodTruckId = generateUuidV4();
-		$location = new Location($locationId, $this->location->getLocationId(), $locationFoodTruckId, $this->location->getLocationFoodTruckId(), $this->VALID_LOCATIONENDTIME, $this->VALID_LOCATIONLATITUDE, $this->VALID_LOCATIONLONGITUDE, $this->VALID_LOCATIONSTARTTIME);
+		$location = new Location($locationId, $this->foodTruck->getFoodTruckId(), $this->VALID_LOCATIONENDTIME, $this->VALID_LOCATIONLATITUDE, $this->VALID_LOCATIONLONGITUDE, $this->VALID_LOCATIONSTARTTIME);
 		$location->insert($this->getPDO());
 
 		// grab the data from mySQL and enforce the fields match our expectations
