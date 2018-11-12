f3783a2c095f5f213d7c4cb2b55971fcbca7eca4
Greg Klein <gregkleinwrites@gmail.com>
2018-11-12 14:13:14 -0700

    changed name of encrypted property file
    url-8179

--- php/classes/Test/FoodTruckFinderTestSetup.php
+++ php/classes/Test/FoodTruckFinderTestSetup.php
@@ -8,7 +8,7 @@ use PHPUnit\DbUnit\Database\Connection;
 use PHPUnit\DbUnit\Operation\{Composite, Factory, Operation};
 
 // grab the encrypted properties file
-require_once("/etc/apache2/capstone-mysql/Secret.php");
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
 
 require_once(dirname(__DIR__, 3) . "/vendor/autoload.php");
 
