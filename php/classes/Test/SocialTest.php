5df7507183f40d5cb7b6af86bb64ef731a680f7b
Brittany jack <jackbrittany54@yahoo.com>
2018-11-15 13:28:17 -0700

    Added uuid to social test line 215 because error said social url was empty 8183

--- php/classes/Test/SocialTest.php
+++ php/classes/Test/SocialTest.php
@@ -212,7 +212,7 @@ class SocialTest extends FoodTruckFinderTest {
 
 		// create a new social and insert to into mySQL
 		$socialId = generateUuidV4();
-		$social = new Social($socialId, $this->social->getSocialId(), $this->VALID_SOCIAL_FOOD_TRUCK_ID_, $this->VALID_SOCIAL_URL);
+		$social = new Social($socialId, $this->social->getSocialId(), $this->VALID_SOCIAL_FOOD_TRUCK_ID_, $this->VALID_SOCIAL_URL,"690a781c-4246-4719-82ea-5cd435602bcf");
 		$social->insert($this->getPDO());
 
 		// grab the data from mySQL and enforce the fields match our expectations
