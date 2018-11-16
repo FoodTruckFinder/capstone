f2076717a55e8840bf672aae5d0f20ed1c1456a0
Greg Klein <gregkleinwrites@gmail.com>
2018-11-16 10:35:10 -0700

    starting on rest API for foodTruck
            url-8179

--- public_html/api/foodTruck/index.php
+++ public_html/api/foodTruck/index.php
@@ -1,1 +1,30 @@
-test
+<?php
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use FoodTruckFinder\Capstone\Profile;
+use FoodTruckFinder\Capstone\FoodTruck;
+
+// GET request
+if ($method === "GET") {
+
+//if it's not a GET request, we determine if we have a PUT or POST request
+} else if($method === "PUT" || $method === "POST") {
+
+	//do setup needed for PUT and POST
+	if($method === "PUT") {
+		//determine if we have a PUT request. Process PUT request here
+
+	} else if ($method === "POST") {
+		//process POST request here
+
+	}
+
+	//if above requests are neither PUT nor POST, use DELETE below
+} else if($method === "DELETE") {
+
+	//process DELETE request
+}
