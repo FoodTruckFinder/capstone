fbed5461ad96b4c679e4ca19d7bd8062c7bf1c9c
Greg Klein <gregkleinwrites@gmail.com>
2018-11-20 14:37:04 -0700

    fixed an empty comment causing an erorr in david's mainpage html

--- public_html/api/foodTruck/index.php
+++ public_html/api/foodTruck/index.php
@@ -148,6 +148,15 @@ if ($method === "GET") {
 	$foodTruck->delete($pdo);
 	// update reply
 	$reply->message = "Food Truck deleted OK";
+} else {
+	throw (new InvalidArgumentException("Invalid HTTP method request"));
+}
+// update the $reply->status $reply->message
+} catch(\Exception | \TypeError $exception) {
+	$reply->status = $exception->getCode();
+	$reply->message = $exception->getMessage();
 }
 
-}
+// encode and return reply to front end caller
+header("Content-type: application/json");
+echo json_encode($reply);
