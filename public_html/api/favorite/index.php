9bd6439aa7bca0492cef902ad8fdc970609255ba
Bernina Gray <bernina.gray@icloud.com>
2018-11-20 16:32:38 -0700

    8181 Added pdo up to line 35 with ini file.

---
+++ public_html/api/favorite/index.php
@@ -0,0 +1,36 @@
+<?php
+
+require_once dirname(__3__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR, 3) . "/php/classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
+
+use FoodTruckFinder\Capstone\Favorite;
+use FoodTruckFinder\Capstone\FoodTruck;
+
+};
+
+/**
+ * Api for the Favorite class
+ *
+ * @author bernina gray
+ **/
+
+// verify the session, start if not active
+if(session_status() !== PHP_SESSION_ACTIVE) {
+	session_start();
+}
+//prepare an empty reply
+$reply = new stdClass();
+$reply->status = 200;
+$reply->data = null;
+try {
+		$pdo = connectToEncryptedMySQL();
+
+		// determine which HTTP method was used
+		$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
+
+		// sanitize
+
