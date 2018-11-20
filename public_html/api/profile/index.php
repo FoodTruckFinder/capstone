428a96696ff9533090cafa53003c4346f8ca6f72
Daniel Nakitare <dnakitare@cnm.edu>
2018-11-20 16:17:02 -0700

    Profile API building GET and PUT

--- public_html/api/profile/index.php
+++ public_html/api/profile/index.php
@@ -34,12 +34,50 @@ try {
 
 	// sanitize input
 	$profileId = filter_input(INPUT_GET, "profileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+	$profileActivationToken = filter_input(INPUT_GET, "profileActivationToken", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$profileEmail = filter_input(INPUT_GET, "profileEmail", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
-	$profileName = filter_input(INPUT_GET, "profileName", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
+
 
 	// make sure the profile id is valid for methods that require it
 	if(($method === "DELETE" || $method === "PUT") && (empty($profileId) === true)) {
 		throw(new \InvalidArgumentException("profile id cannot be empty or negative", 405));
+	}
+
+	if($method === "GET") {
+		// set XSRF cookie
+		setXsrfCookie();
+
+		// get a Profile by content
+		if(empty($profileId) === false) {
+			$reply->data = Profile::getProfileByProfileId($pdo, $profileId);
+		} elseif(empty($profileActivationToken) === false) {
+			$reply->data = Profile::getProfileByProfileActivationToken($pdo, $profileActivationToken);
+		} elseif(empty($profileEmail) == false) {
+			$reply->data = Profile::getProfileByProfileEmail($pdo, $profileEmail);
+		}
+	} elseif($method === "PUT") {
+		// enforce that the XSRF token is present in the header
+		verifyXsrf();
+
+		// enforce the user is signed in and only trying to edit their own profile
+		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $profileId) {
+			throw (new \InvalidArgumentException("You are not allowed to access this profile", 403));
+		}
+
+		validateJwtHeader();
+
+		// decode the response from the front end
+		$requestContent = file_get_contents("php://input");
+		$requestObject = json_decode($requestContent);
+
+
+		// retrieve the profile to be updated
+		$profile = Profile::getProfileByProfileId($pdo, $profileId);
+		if($profile === null) {
+			throw (new \RuntimeException("Profile does not exist", 404));
+		}
+
+		// profile
 	}
 }
 
