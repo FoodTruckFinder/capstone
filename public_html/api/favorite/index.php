7e2af43044df01ea0d5f497dc53a8e644cd2904b
Bernina Gray <bernina.gray@icloud.com>
2018-11-20 16:32:59 -0700

    8181 Added pdo up to line 35 with ini file.

--- public_html/api/favorite/index.php
+++ public_html/api/favorite/index.php
@@ -27,7 +27,7 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 try {
-		$pdo = connectToEncryptedMySQL();
+		$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort22/fooddelivery.ini");
 
 		// determine which HTTP method was used
 		$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
