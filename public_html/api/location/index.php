fbae25cc052f7a09c2b3c714f19b4274fdbd8dfa
dsanderson90 <dsanderson3@cnm.edu>
2018-11-25 12:22:18 -0700

    8182 - wrote the delete method

--- public_html/api/location/index.php
+++ public_html/api/location/index.php
@@ -5,10 +5,12 @@ require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
-use \FoodTruckFinder\Capstone\Location;
+use \FoodTruckFinder\Capstone\{Location, Profile, FoodTruck};
 
 /**
  * * api for Location class
+ *
+ * GET, POST, PUT, and DELETE
  *
  * @author David Sanderson sanderdj90@gmail.com
  **/
@@ -33,9 +35,16 @@ try {
 	$locationLatitude = filter_input(INPUT_GET, "locationLatitude", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 	$locationLongitude = filter_input(INPUT_GET, "locationLongitude", FILTER_VALIDATE_FLOAT, FILTER_FLAG_ALLOW_FRACTION);
 	// handle GET request - if locationId or locationFoodTruckId is present, that location is returned, otherwise all location are returned.
+
+	//make sure the locationID is valid for methods that need it
+	if(($method === "DELETE" || $method === "PUT") && empty($locationId) === true) {
+		throw(new InvalidArgumentException("id cannot be empty", 405));
+	}
+
 	if($method === "GET") {
 		//set XSRF cookie
 		setXsrfCookie();
+
 		//get a specific location by locationId, locationFoodTruckId or return all locations
 
 		if(empty($locationId) === false) {
@@ -57,7 +66,49 @@ try {
 			}
 		}
 		// If the method request is not GET an invalid argument exception is thrown
-	} else {
+	} else if($method === "PUT" || $method === "POST") {
+
+
+		// enforce the user has a XSRF Token
+		verifyXsrf();
+
+		// Retrieves the JSON pkg that the front end sent, and stores it in $requestContent.
+
+		$requestContent = file_get_contents("php://input");
+
+		// This line then decodes the JSON pkg and stores that result in $requestObject
+
+		$requestObject = json_decode($requestContent);
+
+		//make sure the location
+
+	}
+
+	else if ($method === "DELETE") {
+
+		//enforce the user has a XSRF Token
+		verifyXsrf();
+
+		//retrieve the Location to be deleted
+		$location = Location::getLocationByLocationId($pdo, $locationId);
+		if($location === null) {
+					throw(new RuntimeException("Location not found", 404));
+		}
+
+		//enforce the user is signed in and trying to to edit their own location
+
+		//TODO is there  foodtruck session? How do I enforce the foodtruck owner is editing their own location? Add is owner to this if block?
+		if(empty($_SESSION["profile"]) === true || $_SESSION["foodtruck"]->getFoodTruckId()->toString() !== $location->getLocationFoodTruckId()->toString()) {
+					throw(new \InvalidArgumentException("You are not allowed to delete this location", 403));
+	}
+	//delete location
+	$location->delete($pdo);
+	// update reply
+	$reply->message = "Location Deleted.";
+
+
+}
+else {
 		throw (new InvalidArgumentException("Invalid HTTP Method Request", 418));
 	}
 // update reply with exception information
