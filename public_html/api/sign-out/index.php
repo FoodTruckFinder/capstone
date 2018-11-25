ddd5ba279656ce15a905f16a8d0d6489a4a20da5
Brittany jack <jackbrittany54@yahoo.com>
2018-11-25 14:18:25 -0700

    added use foodtruckfinder 8183

--- public_html/api/sign-out/index.php
+++ public_html/api/sign-out/index.php
@@ -2,6 +2,9 @@
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once ("/etc/apache2/capstone-mysql/encrypted-config.php");
+
+use \FoodTruckFinder\Capstone\Profile;
+
 /**
  * api for signing out
  *
