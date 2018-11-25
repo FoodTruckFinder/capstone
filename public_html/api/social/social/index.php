aa2f34b10d03b9e7de23bece065c853eb1c385c6
Brittany jack <jackbrittany54@yahoo.com>
2018-11-25 14:22:12 -0700

    fixed typo 8183

--- public_html/api/social/social/index.php
+++ public_html/api/social/social/index.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once dirname(__3__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
 require_once dirname(__DIR, 3) . "/php/classes/autoload.php";
 require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
