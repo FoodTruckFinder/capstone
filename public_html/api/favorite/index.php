248eab322c03f8dc6f8c91a0d3e5a873a7dff3d3
Bernina Gray <bernina.gray@icloud.com>
2018-11-26 16:59:00 -0700

    4786 commit new changes

--- public_html/api/favorite/index.php
+++ public_html/api/favorite/index.php
@@ -1,8 +1,8 @@
 <?php
 
-require_once dirname(__3__, 3) . "/vendor/autoload.php";
-require_once dirname(__DIR, 3) . "/php/classes/autoload.php";
-require_once("/etc/apache2/capstone-mysql/encrypted-config.php");
+require_once dirname(__DIR__, 3) . "/vendor/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once("/etc/apache2/capstone-mysql/Secrets.php");
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
 require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
 require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
@@ -10,7 +10,6 @@ require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 use FoodTruckFinder\Capstone\Favorite;
 use FoodTruckFinder\Capstone\FoodTruck;
 
-};
 
 /**
  * Api for the Favorite class
@@ -27,8 +26,8 @@ $reply = new stdClass();
 $reply->status = 200;
 $reply->data = null;
 try {
-		$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/cohort22/fooddelivery.ini");
-
+	$secrets =  new \Secrets("/etc/apache2/capstone-mysql/cohort22/fooddelivery");
+	$pdo = $secrets->getPdoObject();
 		// determine which HTTP method was used
 		$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
