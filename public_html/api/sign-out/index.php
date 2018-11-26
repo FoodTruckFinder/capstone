acba5b6c269901efa95c9518d4b3ebff5788b7b5
Brittany jack <jackbrittany54@yahoo.com>
2018-11-26 16:02:24 -0700

    added more namespace to sign-out 4783

--- public_html/api/sign-out/index.php
+++ public_html/api/sign-out/index.php
@@ -1,6 +1,9 @@
 <?php
 require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
+require_once dirname(__DIR__, 3) . "/php/classes/autoload.php";
 require_once dirname(__DIR__, 3) . "/php/lib/xsrf.php";
+require_once dirname(__DIR__, 3) . "/php/lib/jwt.php";
+require_once dirname(__DIR__, 3) . "/php/lib/uuid.php";
 require_once ("/etc/apache2/capstone-mysql/secrets.php");
 
 use \FoodTruckFinder\Capstone\Profile;
