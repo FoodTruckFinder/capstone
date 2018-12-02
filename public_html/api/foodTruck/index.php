ae2cac5646bbe69b5296eea0908fc4453113af86
dsanderson90 <dsanderson3@cnm.edu>
2018-12-02 11:50:14 -0700

    8182 - added profile session and removed extra bracket

--- public_html/api/foodTruck/index.php
+++ public_html/api/foodTruck/index.php
@@ -26,13 +26,16 @@ $reply->data = null;
 
 try {
 	//grab the mySQL connection
-	$pdo = connectToEncryptedMySQL("/etc/apache2/capstone-mysql/fooddelivery.ini");
+	$secrets = new \Secrets("/etc/apache2/capstone-mysql/cohort22/fooddelivery");
+	$pdo = $secrets->getPdoObject();
+
+	$_SESSION["profile"] = Profile::getProfileByProfileId($pdo, "b3200b81-2cdd-47dc-9e8e-21f9bd69fe3b");
 
 	//determine which HTTP method was used
 	$method = $_SERVER["HTTP_X_HTTP_METHOD"] ?? $_SERVER["REQUEST_METHOD"];
 
 	//sanitize input
-	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING,FILTER_FLAG_NO_ENCODE_QUOTES);
+	$id = filter_input(INPUT_GET, "id", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$foodTruckProfileId = filter_input(INPUT_GET, "foodTruckProfileId", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$foodTruckDescription = filter_input(INPUT_GET, "foodTruckDescription", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
 	$foodTruckImageUrl = filter_input(INPUT_GET, "foodTruckImageUrl", FILTER_SANITIZE_STRING, FILTER_FLAG_NO_ENCODE_QUOTES);
@@ -46,111 +49,111 @@ try {
 	}
 
 // GET request
-if ($method === "GET") {
 	if($method === "GET") {
-		//set XSRF cookie
-		setXsrfCookie();
+		if($method === "GET") {
+			//set XSRF cookie
+			setXsrfCookie();
 
-		//get a specific foodTruck based on arguments provided or all the foodTrucks and update reply
-		if(empty($id) === false) {
-			$reply->data = FoodTruck::getFoodTruckByFoodTruckId($pdo, $id);
-		} else if(empty($foodTruckProfileId) === false) {
-			$reply->data = FoodTruck::getFoodTruckByFoodTruckProfileId($pdo, $foodTruckProfileId)->toArray();
-		} else if(empty($foodTruckDescription) === false) {
-			$reply->data = FoodTruck::getFoodTruckByFoodTruckName($pdo, $foodTruckName)->toArray();
-		} else {
-			$reply->data = FoodTruck::getAllFoodTrucks($pdo)->toArray();
+			//get a specific foodTruck based on arguments provided or all the foodTrucks and update reply
+			if(empty($id) === false) {
+				$reply->data = FoodTruck::getFoodTruckByFoodTruckId($pdo, $id);
+			} else if(empty($foodTruckProfileId) === false) {
+				$reply->data = FoodTruck::getFoodTruckByFoodTruckProfileId($pdo, $foodTruckProfileId)->toArray();
+			} else if(empty($foodTruckDescription) === false) {
+				$reply->data = FoodTruck::getFoodTruckByFoodTruckName($pdo, $foodTruckName)->toArray();
+			} else {
+				$reply->data = FoodTruck::getAllFoodTrucks($pdo)->toArray();
+			}
 		}
-	}
 
 
 //if it's not a GET request, we determine if we have a PUT or POST request
-} else if($method === "PUT" || $method === "POST") {
-	// enforce the user has a XSRF token
-	verifyXsrf();
+	} else if($method === "PUT" || $method === "POST") {
+		// enforce the user has a XSRF token
+		verifyXsrf();
 
-	//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
-	$requestContent = file_get_contents("php://input");
+		//  Retrieves the JSON package that the front end sent, and stores it in $requestContent. Here we are using file_get_contents("php://input") to get the request from the front end. file_get_contents() is a PHP function that reads a file into a string. The argument for the function, here, is "php://input". This is a read only stream that allows raw data to be read from the front end request which is, in this case, a JSON package.
+		$requestContent = file_get_contents("php://input");
 
-	// This Line Then decodes the JSON package and stores that result in $requestObject
-	$requestObject = json_decode($requestContent);
+		// This Line Then decodes the JSON package and stores that result in $requestObject
+		$requestObject = json_decode($requestContent);
 
-	//make sure food truck name is available (required field)
-	if(empty($requestObject->foodTruckName) === true) {
-		throw(new \InvalidArgumentException ("No name for food truck.", 405));
-	}
+		//make sure food truck name is available (required field)
+		if(empty($requestObject->foodTruckName) === true) {
+			throw(new \InvalidArgumentException ("No name for food truck.", 405));
+		}
 
-	//  make sure profileId is available
-	if(empty($requestObject->foodTruckProfileId) === true) {
-		throw(new \InvalidArgumentException ("No food truck profile ID.", 405));
-	}
-	if($method === "PUT") {
-		//determine if we have a PUT request. Process PUT request here
-		// retrieve the food Truck to update
+		//  make sure profileId is available
+		if(empty($requestObject->foodTruckProfileId) === true) {
+			throw(new \InvalidArgumentException ("No food truck profile ID.", 405));
+		}
+		if($method === "PUT") {
+			//determine if we have a PUT request. Process PUT request here
+			// retrieve the food Truck to update
 
+			$foodTruck = FoodTruck::getFoodTruckByFoodTruckId($pdo, $id);
+			if($foodTruck === null) {
+				throw(new RuntimeException("Food truck does not exist", 404));
+			}
+
+			//enforce the user is signed in and only trying to edit their own tweet
+			if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $foodTruck->getFoodTruckProfileId()->toString()) {
+				throw(new \InvalidArgumentException("You are not allowed to edit this food truck", 403));
+			}
+
+			// update all attributes
+			$foodTruck->setFoodTruckDescription($requestObject->foodTruckDescription);
+			$foodTruck->setFoodTruckImageUrl($requestObject->foodTruckImageUrl);
+			$foodTruck->setFoodTruckMenuUrl($requestObject->foodTruckMenuUrl);
+			$foodTruck->setFoodTruckName($requestObject->foodTruckName);
+			$foodTruck->setFoodTruckPhoneNumber($requestObject->foodTruckPhoneNumber);
+			$foodTruck->update($pdo);
+
+			// update reply
+			$reply->message = "Food truck updated OK";
+
+
+		} else if($method === "POST") {
+			//process POST request here
+			// enforce the user is signed in
+
+			if(empty($_SESSION["profile"]) === true) {
+				throw(new \InvalidArgumentException("you must be logged in to edit food trucks", 403));
+			}
+
+			// create new food truck and insert into the database
+			$foodTruck = new FoodTruck(generateUuidV4(), $_SESSION["profile"]->getProfileId, $requestObject->foodTruckDescription, $requestObject->foodTruckImageUrl, $requestObject->foodTruckMenuUrl, $requestObject->foodTruckName, $requestObject->foodTruckPhoneNumber);
+			$foodTruck->insert($pdo);
+
+			// update reply
+			$reply->message = "Food Truck created OK";
+		}
+
+
+		//if above requests are neither PUT nor POST, use DELETE below
+	} else if($method === "DELETE") {
+		//process DELETE request
+		//enforce that the end user has a XSRF token.
+		verifyXsrf();
+
+		// retrieve the food truck to be deleted
 		$foodTruck = FoodTruck::getFoodTruckByFoodTruckId($pdo, $id);
 		if($foodTruck === null) {
 			throw(new RuntimeException("Food truck does not exist", 404));
 		}
 
-		//enforce the user is signed in and only trying to edit their own tweet
-		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId()->toString() !== $foodTruck->getFoodTruckProfileId()->toString()) {
-			throw(new \InvalidArgumentException("You are not allowed to edit this food truck", 403));
+		//enforce the user is signed in and only trying to edit their own food truck
+		if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== $foodTruck->getFoodTruckProfileId()) {
+			throw(new \InvalidArgumentException("You are not allowed to delete this food truck", 403));
 		}
 
-		// update all attributes
-		$foodTruck->setFoodTruckDescription($requestObject->foodTruckDescription);
-		$foodTruck->setFoodTruckImageUrl($requestObject->foodTruckImageUrl);
-		$foodTruck->setFoodTruckMenuUrl($requestObject->foodTruckMenuUrl);
-		$foodTruck->setFoodTruckName($requestObject->foodTruckName);
-		$foodTruck->setFoodTruckPhoneNumber($requestObject->foodTruckPhoneNumber);
-		$foodTruck->update($pdo);
-
+		// delete foodtruck //TODO add cascading deletes to delete all related foodtruck records favorite -> social -> location KILL THE CHILDREN
+		$foodTruck->delete($pdo);
 		// update reply
-		$reply->message = "Food truck updated OK";
-
-
-	} else if ($method === "POST") {
-		//process POST request here
-		// enforce the user is signed in
-
-		if(empty($_SESSION["profile"]) === true) {
-			throw(new \InvalidArgumentException("you must be logged in to edit food trucks", 403));
-		}
-
-		// create new food truck and insert into the database
-		$foodTruck = new FoodTruck(generateUuidV4(), $_SESSION["profile"]->getProfileId, $requestObject->foodTruckDescription, $requestObject->foodTruckImageUrl, $requestObject->foodTruckMenuUrl, $requestObject->foodTruckName, $requestObject->foodTruckPhoneNumber);
-		$foodTruck->insert($pdo);
-
-		// update reply
-		$reply->message = "Food Truck created OK";
+		$reply->message = "Food Truck deleted OK";
+	} else {
+		throw (new InvalidArgumentException("Invalid HTTP method request"));
 	}
-	}
-
-	//if above requests are neither PUT nor POST, use DELETE below
-} else if($method === "DELETE") {
-	//process DELETE request
-	//enforce that the end user has a XSRF token.
-	verifyXsrf();
-
-	// retrieve the food truck to be deleted
-	$foodTruck = FoodTruck::getFoodTruckByFoodTruckId($pdo, $id);
-	if($foodTruck === null) {
-		throw(new RuntimeException("Food truck does not exist", 404));
-	}
-
-	//enforce the user is signed in and only trying to edit their own food truck
-	if(empty($_SESSION["profile"]) === true || $_SESSION["profile"]->getProfileId() !== $foodTruck->getFoodTruckProfileId()) {
-		throw(new \InvalidArgumentException("You are not allowed to delete this food truck", 403));
-	}
-
-	// delete tweet
-	$foodTruck->delete($pdo);
-	// update reply
-	$reply->message = "Food Truck deleted OK";
-} else {
-	throw (new InvalidArgumentException("Invalid HTTP method request"));
-}
 // update the $reply->status $reply->message
 } catch(\Exception | \TypeError $exception) {
 	$reply->status = $exception->getCode();
