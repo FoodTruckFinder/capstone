8e95735cc6c27105ac4b4c0c5addc50d41c52365
Daniel Nakitare <dnakitare@cnm.edu>
2018-12-04 14:13:32 -0700

    added update-profile for static ui

---
+++ static-ui/update-profile.php
@@ -0,0 +1,17 @@
+
+
+
+
+<main>
+	<div class="container-fluid">
+		<h1 class="text-center">Update Profile</h1>
+		<form class="form-control-lg" id="form" action="" method="post">
+			<div class="info">
+				<input class="form-control" id="email" type="email" name="email" placeholder=" Email"/>
+				<input class="form-control" id="username" type="text" name="password" placeholder=" Password"/>
+			</div>
+			<button type="button" class="btn pull-right m-2" data-dismiss="modal">Cancel</button>
+			<button type="button" class="btn green pull-right m-2">Save changes</button>
+		</form>
+	</div>
+</main>
