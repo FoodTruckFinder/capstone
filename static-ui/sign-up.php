83246e8213aed14cad44f6949ba9d020dd81d78a
Greg Klein <shiberino@outlook.com>
2018-12-04 14:14:24 -0700

    reworking the sign-up.php

--- static-ui/sign-up.php
+++ static-ui/sign-up.php
@@ -1,39 +1,49 @@
-<!DOCTYPE html>
-<html lang="en">
-	<head>
-		<meta charset="UTF-8">
-	</head>
-	<body>
-		<main>
-			<ngb-tabset>
-				<ngb-tab title="Customer Sign Up">
-					<ng-template ngbTabContent>
 
-						<!--insert form-->
+<body>
+	<main>
+		<div class="container text-center">
+			<h1>Sign Up for Food  Truck Finder</h1>
+		</div>
 
-					</ng-template>
-				</ngb-tab>
-				<ngb-tab>
-					<ng-template ngbTabTitle><b>Food Truck Owner Signup</b></ng-template>
-					<ng-template ngbTabContent>Food truck fixie locavore, accusamus mcsweeney's marfa nulla single-origin coffee squid.
+		<ngb-tabset>
+			<ngb-tab title="Customer Sign Up">
+				<ng-template ngbTabContent>
 
-						<!--insert form-->
+					<form class="form-control-lg" id="form" action="" method="post">
+						<div class="info">
+							<input class="form-control" id="name" type="text" name="name" placeholder=" Name"/>
+							<input class="form-control" id="email" type="email" name="email" placeholder=" Email Address"/>
+							<input class="form-control" id="password" type="text" name="password" placeholder=" Password">
+							<input class="form-control" id="password-confirm" type="text" name="password-confirm" placeholder=" Re-enter Password">
+							<input class="btn" type="submit" value="Sign Up as Customer">
+						</div>
+					</form>
 
-					</ng-template>
-				</ngb-tab>
-			</ngb-tabset>
-			<div class="container text-center">
-				<h1>Sign Up for Food  Truck Finder</h1>
-				<form class="form-control-lg" id="form" action="" method="post">
-					<div class="info">
-						<input class="form-control" id="name" type="text" name="name" placeholder=" User Name"/>
-						<input class="form-control" id="email" type="email" name="email" placeholder=" Email"/>
-						<input class="form-control" id="password" type="text" name="password" placeholder=" Password">
-						<input class="form-control" id="password-confirm" type="text" name="password-confirm" placeholder=" Re-enter Password">
-						<input class="btn" type="submit" value="Sign Up">
-					</div>
-				</form>
-			</div>
+					<!--toDo add code that marks this sign up as being from a customer-->
+
+				</ng-template>
+			</ngb-tab>
+			<ngb-tab>
+				<ng-template ngbTabTitle><b>Food Truck Owner Signup</b></ng-template>
+				<ng-template ngbTabContent>
+
+					<form class="form-control-lg" id="form" action="" method="post">
+						<div class="info">
+							<input class="form-control" id="name" type="text" name="name" placeholder=" Name"/>
+							<input class="form-control" id="email" type="email" name="email" placeholder=" Email Address"/>
+							<input class="form-control" id="password" type="text" name="password" placeholder=" Password">
+							<input class="form-control" id="password-confirm" type="text" name="password-confirm" placeholder=" Re-enter Password">
+							<input class="btn" type="submit" value="Sign Up as Food Truck Owner">
+						</div>
+					</form>
+
+					<!--toDo add code that marks this sign up as being from an owner, not a customer-->
+
+				</ng-template>
+			</ngb-tab>
+
+		</ngb-tabset>
+
 		</main>
 	</body>
 </html>
