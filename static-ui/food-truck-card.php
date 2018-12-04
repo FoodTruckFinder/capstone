165908a405b950db7e9a3907c8b77edd9440ec7d
Greg Klein <shiberino@outlook.com>
2018-12-04 13:29:31 -0700

    added php file for listing food truck cards (if we let them be searchable)

---
+++ static-ui/food-truck-card.php
@@ -0,0 +1,18 @@
+<?php require_once ("head-utils.php");?>
+
+<?php require_once("navbar.php");?>
+
+<main>
+	<div class="container-fluid p-2">
+		<div class="card">
+			<!--toDo: add image URL for the cards-->
+			<img class="card-img-top p-1" src="foodTruckImageUrl" alt="image for food truck">
+			<ul class="p-1 list-group list-group-flush">
+				<li class="list-group-item">Food Truck Name</li>
+				<li class="list-group-item">Food Truck description</li>
+				<li class="list-group-item">Food Truck Menu URL</li>
+				<li class="list-group-item">Food Truck Phone #</li>
+			</ul>
+		</div>
+	</div>
+</main>
