82130b7c2ba026b84b521c77bef027a285e28b25
Greg Klein <shiberino@outlook.com>
2018-12-04 14:18:14 -0700

    making the head-utils.php

---
+++ static-ui/head-utils.php
@@ -0,0 +1,14 @@
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+	<!-- insert google font -->
+
+
+	<!-- our custom CSS style -->
+	<link rel="stylesheet" href="css/style.css" type="text/css">
+
+	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
+	<title>Food Truck Finder</title>
+</head>
