6181ee139dcaa46a35d1319ef5901834e3481704
Greg Klein <shiberino@outlook.com>
2018-12-05 09:30:55 -0700

    working on the about us page css @asana 0174

--- static-ui/about.php
+++ static-ui/about.php
@@ -8,27 +8,29 @@
 		-->
 
 		<!--referencing the styles needed-->
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 		<!--toDO require_once not currently working will fix later php require_once("head_utils.php"); -->
 
 
 		<title>About Us - Food Truck Finder</title>
 	<body>
-		<h1>What is Food Truck Finder?</h1>
+		<div class="aboutus">
+			<h1>What is Food Truck Finder?</h1>
 
-		<!--toDo list people from left to right according to team photo-->
-		<p>Food Truck Finder was built by (toDo list people from left to right according to team photo)</p>
+			<!--toDo list people from left to right according to team photo-->
+			<p>Food Truck Finder was built by (toDo list people from left to right according to team photo)</p>
 
-		<p>Food Truck Finder is a web app designed to help food truck users easily find the best food nearby as well as a resource for food truck owners to let their fans know when they are open for business, what items are on the menu, and more. Albuquerque has great food, and we've created an app that helps both locals and tourists find the best food as quickly as possible.</p>
+			<p>Food Truck Finder is a web app designed to help food truck users easily find the best food nearby as well as a resource for food truck owners to let their fans know when they are open for business, what items are on the menu, and more. Albuquerque has great food, and we've created an app that helps both locals and tourists find the best food as quickly as possible.</p>
 
-		<h2>The Food Truck 5 Team</h2>
+			<h2>The Food Truck 5 Team</h2>
 
-		<!--toDo add image of team-->
-		<img class="teamphoto" src="https://www.dropbox.com/s/qs9g8hm868j3ooa/teamphoto.png?raw=1" alt="image of the members of the food truck finder team">
+			<!--toDo add image of team-->
+			<img class="teamphoto" src="https://www.dropbox.com/s/qs9g8hm868j3ooa/teamphoto.png?raw=1" alt="image of the members of the food truck finder team">
 
-		<h2>Check out the Food Truck Finder GitHub Repo</h2>
-		<a href="https://github.com/FoodTruckFinder"><i class="fab fa-github"></i></a>
-		<h2>Contact Info</h2>
-		<!--toDo list member contact info as cards in order of appearance on the image--one row on desktop, column on mobile. Card will contain headshot, github profile link (as icon), link to PWP, link to social media channels-->
-
+			<h2>Check out the Food Truck Finder GitHub Repo</h2>
+			<a href="https://github.com/FoodTruckFinder"><i class="fab fa-github"></i></a>
+			<h2>Contact Info</h2>
+			<!--toDo list member contact info as cards in order of appearance on the image--one row on desktop, column on mobile. Card will contain headshot, github profile link (as icon), link to PWP, link to social media channels-->
+		</div>
 	</body>
 </html>
