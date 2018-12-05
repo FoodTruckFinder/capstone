86deeabc2a75bab23afc6c8157cba5894c1f1ac5
dsanderson90 <dsanderson3@cnm.edu>
2018-12-05 15:05:28 -0700

    8182 - added the session service which connects to earl greys api to set the session's xsrf token

---
+++ src/app/shared/services/session.service.ts
@@ -0,0 +1,17 @@
+import { Injectable } from "@angular/core";
+import { HttpClient } from "@angular/common/http";
+import { Status } from "../interfaces/status";
+import { Observable } from "rxjs/internal/Observable";
+
+@Injectable()
+
+export class SessionService {
+
+	constructor(protected http:HttpClient) {}
+
+	private sessionUrl = "api/earl-grey/";
+
+	setSession() {
+		return (this.http.get<Status>(this.sessionUrl, {}));
+	}
+}
