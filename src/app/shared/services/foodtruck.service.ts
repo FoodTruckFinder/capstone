bc0aae5aa84940ff843e8da6633629f16d41c914
Daniel Nakitare <dnakitare@cnm.edu>
2018-12-05 14:32:19 -0700

    created foodtruck.service.ts in src/app/shared/services

---
+++ src/app/shared/services/foodtruck.service.ts
@@ -0,0 +1,45 @@
+import {HttpClient} from "@angular/common/http";
+import {Injectable} from "@angular/core";
+import {Observable} from "rxjs";
+import {FoodTruck} from "../interfaces/foodtruck";
+import {Status} from "../interfaces/status";
+
+@Injectable()
+export class FoodTruckService {
+	constructor(protected http: HttpClient) {}
+
+	// define an API endpoint
+	private foodtruckUrl = "api/foodtruck";
+
+	// reach out to the foodtruck API and create the foodtruck
+	createFoodTruck(foodtruck: FoodTruck) : Observable<Status> {
+		return(this.http.post<Status>(this.foodtruckUrl, foodtruck));
+	}
+
+	// reach out to the foodtruck API and edit the foodtruck
+	editFoodTruck(foodtruck: FoodTruck) : Observable<Status> {
+		return(this.http.put<Status>(this.foodtruckUrl, foodtruck));
+	}
+
+	// reach out to the foodtruck API and grab a FoodTruck object by it's Id
+	getFoodTruckByFoodTruckId(foodTruckId: string) : Observable<FoodTruck> {
+		return(this.http.get<FoodTruck>(this.foodtruckUrl + foodTruckId));
+	}
+
+	// reach out to the foodtruck API and grab a FoodTruck object by it's FoodTruck Profile Id
+	getFoodTruckByFoodtruckProfileId(foodTruckProfileId: string) : Observable<FoodTruck> {
+		return(this.http.get<FoodTruck>(this.foodtruckUrl + foodTruckProfileId));
+	}
+
+	// reach out to the foodtruck API and grab a FoodTruck object by it's Name
+	getFoodTruckByFoodtruckName(foodTruckName: string) : Observable<FoodTruck> {
+		return(this.http.get<FoodTruck>(this.foodtruckUrl + foodTruckName));
+	}
+
+	// reach out to the foodtruck API and grab a FoodTruck object by it's FoodTruck Profile Id
+	getAllFoodTrucks(foodtruck: string) : Observable<FoodTruck[]> {
+		return(this.http.get<FoodTruck[]>(this.foodtruckUrl));
+	}
+
+
+}
