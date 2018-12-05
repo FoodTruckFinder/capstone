453b92a7461704fe4e9c491320b7d9723163035d
Daniel Nakitare <dnakitare@cnm.edu>
2018-12-05 10:28:42 -0700

    added foodtruck.ts to src/app/shared/interfaces

---
+++ src/app/shared/interfaces/foodtruck.ts
@@ -0,0 +1,9 @@
+export interface Foodtruck {
+	foodTruckId: string;
+	foodTruckProfileId: string;
+	foodTruckDescription: string;
+	foodTruckImageUrl: string;
+	foodTruckMenuUrl: string;
+	foodTruckName: string;
+	foodTruckPhoneNumber: string;
+}
