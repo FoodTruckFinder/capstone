e38ffcbc67b721e9f73e7fe2b6974bc264cde810
dsanderson90 <dsanderson3@cnm.edu>
2018-12-05 10:08:29 -0700

    8182 - created location interface

---
+++ src/app/shared/interfaces/location.ts
@@ -0,0 +1,8 @@
+export interface Location {
+	locationId: string;
+	locationFoodTruckId: string;
+	locationEndTime: string;
+	locationLatitude: string;
+	locationLongitude: string;
+	locationStartTime: string;
+}
