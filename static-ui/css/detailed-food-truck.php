b2d029f97157d8e26c7354e24cae6b07593191ce
Brittany jack <jackbrittany54@yahoo.com>
2018-12-05 10:44:39 -0700

    changed title from detailed food truck to food truck details 0171

--- static-ui/css/detailed-food-truck.php
+++ static-ui/css/detailed-food-truck.php
@@ -2,7 +2,7 @@
 <html lang="en">
 	<head>
 		<meta charset="UTF-8"/>
-		<title>Detailed Food Truck</title>
+		<title>Food Truck Details</title>
 		<link rel="stylesheet" href="./styles/style.css">
 	</head>
 			<body>
