f2172735dfd158fe07380943d72c9d501c60b7b7
Daniel Nakitare <dnakitare@cnm.edu>
2018-12-05 14:48:01 -0700

    added import statement for Status

--- src/app/shared/services/profile.service.ts
+++ src/app/shared/services/profile.service.ts
@@ -2,6 +2,7 @@ import {HttpClient, HttpParams} from "@angular/common/http";
 import {Injectable} from "@angular/core";
 import {Observable} from "rxjs/internal/Observable";
 import {Profile} from "../interfaces/profile";
+import {Status} from "../interfaces/status";
 
 @Injectable()
 export class ProfileService {
