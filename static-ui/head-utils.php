7be8a2938151db5f81a3a3d568bd63cc1d661541
Greg Klein <shiberino@outlook.com>
2018-12-05 09:13:10 -0700

    working on the about us page
    @asana 0174

--- static-ui/head-utils.php
+++ static-ui/head-utils.php
@@ -1,14 +1,18 @@
 <head>
 	<meta charset="utf-8">
+	<!--responsive website meta-->
 	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
 	<!-- insert google font -->
+	<link href="https://fonts.googleapis.com/css?family=Roboto" rel="stylesheet">
 
+	<!--font awesome stuff for icons and whatnot-->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css" integrity="sha384-B4dIYHKNBt8Bc12p+WXckhzcICo0wtJAoU8YZTY5qE0Id1GSseTk6S+L3BlXeVIU" crossorigin="anonymous">
 
 	<!-- our custom CSS style -->
 	<link rel="stylesheet" href="css/style.css" type="text/css">
 
-	<!-- jQuery first, then Popper.js, then Bootstrap JS -->
+
 
 	<title>Food Truck Finder</title>
 </head>
