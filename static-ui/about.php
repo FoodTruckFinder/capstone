65c1a523d639f9944b3509ddd81067096221568b
Bernina Gray <bernina.gray@icloud.com>
2018-12-06 15:43:24 -0700

    0174 Added div class container to About us php

--- static-ui/about.php
+++ static-ui/about.php
@@ -3,7 +3,7 @@
 <?php require_once("navbar.php");?>
 
 <main>
-	<div>
+	<div class="container text-center">
 		<h1>What is Food Truck Finder?</h1>
 
 			<!--toDo list people from left to right according to team photo-->
