c88ef0d0b3d4140f1941409439e931848176ffab
Daniel Nakitare <dnakitare@cnm.edu>
2018-12-06 16:25:14 -0700

    trying to get everything ready to test sign-up

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,17 +1,21 @@
 import {RouterModule, Routes} from "@angular/router";
 import {APP_BASE_HREF} from "@angular/common";
+import {HomeViewComponent} from "./home-view/home-view.component";
 import {DeepDiveInterceptor} from "./shared/interceptors/deep.dive.interceptor";
 import {HTTP_INTERCEPTORS} from "@angular/common/http";
+import {SignUpService} from "./shared/services/sign.up.service";
+import {SignUpComponent} from "./sign-up/sign-up.component";
 
-export const allAppComponents = [HomeViewComponent];
+export const allAppComponents = [HomeViewComponent, SignUpComponent, SignUpService];
 
 export const routes: Routes = [
-	{path: "", component: SplashComponent}
+	{path: "", component: HomeViewComponent}
 ];
 
 export const appRoutingProviders: any[] = [
 	{provide: APP_BASE_HREF, useValue: window["_base_href"]},
-	{provide: HTTP_INTERCEPTORS, useClass: DeepDiveInterceptor, multi: true}
+	{provide: HTTP_INTERCEPTORS, useClass: DeepDiveInterceptor, multi: true},
+	{provide: SignUpService}
 ];
 
 export const routing = RouterModule.forRoot(routes);
