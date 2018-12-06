42d26639a4ef595214dc196689d5de280bb62620
Brittany jack <jackbrittany54@yahoo.com>
2018-12-06 14:53:19 -0700

    added description and  social media url 0171

--- static-ui/food-truck.php
+++ static-ui/food-truck.php
@@ -3,7 +3,7 @@
 
 
 
-<!--TODO need to add the info and the social media links; no mas-->
+<!--TODO need to add the social media links and menu URL; no mas-->
 
 <main>
 	<div class="container-fluid p-2">
@@ -19,10 +19,10 @@
 				<li class="list-group-item">Food Truck Name: Sanchez Taco Trucks </li>
 				<li class="list-group-item">Food Truck Description:<p>f you want fresh mexican style tacos and more.<br> go to sanchez taco food trucks.
 						They have a fresh variety of burritos, tacos, and tostadas.
-						Friendly<br> service and much more. Please visit Sanchez Food Trucks listed in the locations above.</p></li>
-				<li class="list-group-item">Food Truck Menu URL:</li>
+						Friendly<br> service and much more. Please visit Sanchez Food Trucks listed in the location above.</p></li>
+				<li class="list-group-item">Food Truck Menu URL: </li>
 				<li class="list-group-item">Food Truck Phone:  (505) 489-7194</li>
-				<li class="list-group-item">Social Media Links</li>
+				<li class="list-group-item">Social Media Links: https://www.facebook.com/pages/Sanchez-Tacos/182661148492137</li>
 			</ul>
 		</div>
 	</div>
