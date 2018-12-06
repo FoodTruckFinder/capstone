47c7e1e73022f11a6b081c0e027d27833bbd2610
Brittany jack <jackbrittany54@yahoo.com>
2018-12-05 18:03:02 -0700

    Changed Food Truck html to card 0171

--- static-ui/food-truck-card.php
+++ static-ui/food-truck-card.php
@@ -1,20 +1,26 @@
 <?php require_once ("head-utils.php");?>
 
-<?php require_once("navbar.php");?>
 
 
-<!--toDo we might not need/want this-->
+
+<!--TODO need to add the info and the social media links; no mas-->
 
 <main>
 	<div class="container-fluid p-2">
 		<div class="card">
+			<h1>Sanchez Taco Trucks</h1>
+			<h2>Address: 1607 Broadway Blvd SE - Albuquerque,NM 87105</h2>
+			<p><strong>Open Hours: <br>Mon-Thrus 5pm-12am<br>
+					Fri-Sun 4:15pm-2:00am</strong></p>
+
 			<!--toDo: add image URL for the cards-->
-			<img class="card-img-top p-1" src="foodTruckImageUrl" alt="image for food truck">
+			<img class="sanchez-tacosn" src="sanchez-tacosn%20(2).jpg" alt="sanchez-tacosn(2)">
 			<ul class="p-1 list-group list-group-flush">
-				<li class="list-group-item">Food Truck Name: </li>
-				<li class="list-group-item">Food Truck description:</li>
+				<li class="list-group-item">Food Truck Name: Sanchez Taco Trucks </li>
+				<li class="list-group-item">Food Truck Description:</li>
 				<li class="list-group-item">Food Truck Menu URL</li>
 				<li class="list-group-item">Food Truck Phone #</li>
+				<li class="list-group-item">Social Media Links</li>
 			</ul>
 		</div>
 	</div>
