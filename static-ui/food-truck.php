c10aa36f3265b85ba11e0bd7ec76e86633392e9d
Brittany jack <jackbrittany54@yahoo.com>
2018-12-06 14:20:47 -0700

    Updating description to food truck 0171

--- static-ui/food-truck.php
+++ static-ui/food-truck.php
@@ -9,7 +9,7 @@
 	<div class="container-fluid p-2">
 		<div class="card">
 			<h1>Sanchez Taco Trucks</h1>
-			<h2>Address: 1607 Broadway Blvd SE - Albuquerque,NM 87105</h2>
+			<h2>Address: 8020 Central Ave SE, Albuquerque, NM 87108</h2>
 			<p><strong>Open Hours: <br>Mon-Thrus 5pm-12am<br>
 					Fri-Sun 4:15pm-2:00am</strong></p>
 
@@ -17,9 +17,11 @@
 			<img class="sanchez-tacosn" src="sanchez-tacosn%20(2).jpg" alt="sanchez-tacosn(2)">
 			<ul class="p-1 list-group list-group-flush">
 				<li class="list-group-item">Food Truck Name: Sanchez Taco Trucks </li>
-				<li class="list-group-item">Food Truck Description:</li>
-				<li class="list-group-item">Food Truck Menu URL</li>
-				<li class="list-group-item">Food Truck Phone #</li>
+				<li class="list-group-item">Food Truck Description:<p>f you want fresh mexican style tacos and more.<br> go to sanchez taco food trucks.
+						They have a fresh variety of burritos, tacos, and tostadas.
+						Friendly<br> service and much more. Please visit Sanchez Food Trucks listed in the locations above.</p></li>
+				<li class="list-group-item">Food Truck Menu URL:</li>
+				<li class="list-group-item">Food Truck Phone:  (505) 489-7194</li>
 				<li class="list-group-item">Social Media Links</li>
 			</ul>
 		</div>
