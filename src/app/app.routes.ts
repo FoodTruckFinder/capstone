6e9126ad7c020368a2a6c8fcc7b9baa57de4f06c
Daniel Nakitare <dnakitare@cnm.edu>
2018-12-06 17:26:25 -0700

    made sign-up form work!

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -16,8 +16,8 @@ export const routes: Routes = [
 export const appRoutingProviders: any[] = [
 	{provide: APP_BASE_HREF, useValue: window["_base_href"]},
 	{provide: HTTP_INTERCEPTORS, useClass: DeepDiveInterceptor, multi: true},
-	{provide: SignUpService},
-	SessionService
+	SessionService,
+	SignUpService
 ];
 
 export const routing = RouterModule.forRoot(routes);
