949d1a34efcfbcd3ab1de4e1846eb045f5a2bf2f
Brittany jack <jackbrittany54@yahoo.com>
2018-12-07 08:47:46 -0700

    Added s.footer to static UI 0171

---
+++ static-ui/footer.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		<link rel="stylesheet" href="style.css" type="text/css">
+		<title>Sample Sticky Footer</title>
+	</head>
+	<body class="sfooter">
+
+		<div class="sfooter-content">
+			<!-- your page content here-->
+		</div>
+
+		<footer>
+			<!-- footer here-->
+		</footer>
+
+	</body>
+</html>
