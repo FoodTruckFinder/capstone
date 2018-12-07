99b4c2c664c0fa0ea0c801221a1c8ca4fb37c441
Daniel Nakitare <dnakitare@cnm.edu>
2018-12-06 18:15:53 -0700

    building the sign-in-modal.component.html in shared component html

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -6,8 +6,10 @@ import {HTTP_INTERCEPTORS} from "@angular/common/http";
 import {SignUpService} from "./shared/services/sign.up.service";
 import {SignUpComponent} from "./sign-up/sign-up.component";
 import {SessionService} from "./shared/services/session.service";
+import {SignInService} from "./shared/services/sign.in.service";
+import {SignInComponent} from "./shared/components/sign-in-modal.component";
 
-export const allAppComponents = [HomeViewComponent, SignUpComponent];
+export const allAppComponents = [HomeViewComponent, SignUpComponent, SignInComponent];
 
 export const routes: Routes = [
 	{path: "", component: HomeViewComponent}
@@ -17,7 +19,8 @@ export const appRoutingProviders: any[] = [
 	{provide: APP_BASE_HREF, useValue: window["_base_href"]},
 	{provide: HTTP_INTERCEPTORS, useClass: DeepDiveInterceptor, multi: true},
 	SessionService,
-	SignUpService
+	SignUpService,
+	SignInService
 ];
 
 export const routing = RouterModule.forRoot(routes);
--- src/app/shared/components/sign-in-modal.component.ts
+++ src/app/shared/components/sign-in-modal.component.ts
@@ -1,40 +1,43 @@
 import {Component, ViewChild} from "@angular/core";
-
-import {Router} from "@angular/router";
-import {Status} from "../interfaces/status";
 import {SignIn} from "../interfaces/sign.in";
 import {SignInService} from "../services/sign.in.service";
-import {CookieService} from "ng2-cookies";
+import {Router} from "@angular/router";
+import {Status} from "../interfaces/status";
+import {FormGroup} from "@angular/forms";
 
 
-@Component({
-	template: require("./sign.in.component.html"),
-	selector: "signin"
+@Component ({
+	template: require("./sign-in-modal.component.html"),
+	selector: "sign-in"
 })
 
 export class SignInComponent {
-	@ViewChild("signInForm") signInForm: any;
+	@ViewChild("signInForm")
 
+	signInForm: FormGroup;
 	signin: SignIn = {profileEmail: null, profilePassword: null};
 	status: Status = {status: null, type: null, message: null};
 
-	constructor(private SignInServices: SignInService, private router: Router, private cookieService: CookieService) {
+	constructor(
+		private SignInService: SignInService,
+		private router: Router,
+	)	{}
 
-	}
+
 
 	signIn(): void {
 		localStorage.removeItem("jwt-token");
-		this.SignInServices.postSignIn(this.signin).subscribe(status => {
+		this.SignInService.postSignIn(this.signin).subscribe(status => {
 			this.status = status;
 
+			console.log(status.status);
+
 			if(this.status.status === 200) {
-				this.router.navigate(["/landing-page"]);
+				this.router.navigate(["/profile"]);
 
 			} else {
-				alert("Email or password is incorrect. Try again.")
+				alert("Email or password is incorrect. Please try again.")
 			}
 		});
-
 	}
-
 }
