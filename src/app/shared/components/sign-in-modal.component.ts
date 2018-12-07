e829124d9e48d7934c5be5f01d78fa8eb2d07173
Daniel Nakitare <dnakitare@cnm.edu>
2018-12-07 09:44:12 -0700

    basic sign-in done

--- src/app/shared/components/sign-in-modal.component.ts
+++ src/app/shared/components/sign-in-modal.component.ts
@@ -1,45 +1,37 @@
-import {Component, ViewChild, EventEmitter, Output} from "@angular/core";
+import {Component, OnInit} from "@angular/core";
 import {SignIn} from "../interfaces/sign.in";
 import {SignInService} from "../services/sign.in.service";
-import {Router} from "@angular/router";
+import {FormBuilder, FormGroup, Validators} from "@angular/forms";
 import {Status} from "../interfaces/status";
-import {Observable} from "rxjs"
-import {CookieService} from "ng2-cookies";
-
-declare var $: any;
 
 @Component({
-	template: require( "./sign-in-modal.component.html"),
+	template: require("./sign-in-modal.component.html"),
 	selector: "signin"
 })
 
-export class SignInComponent {
-	@ViewChild("signInForm") signInForm: any;
+export class SignInComponent implements OnInit {
+	signInForm: FormGroup;
+	status: Status = {status: null, message:null, type: null};
 
-	signin: SignIn = {profileEmail: null, profilePassword: null};
-	status: Status = {status: null, type: null, message: null};
-	//cookie: any = {};
+	constructor(private signInService: SignInService, private formBuilder: FormBuilder) {}
 
-	constructor(private SignInService: SignInService, private router: Router, private cookieService : CookieService) {
+	ngOnInit() {
+		this.signInForm = this.formBuilder.group({
+			profileEmail: ["", [Validators.maxLength(128), Validators.required]],
+			profilePassword: ["", [Validators.maxLength(97), Validators.required]]
+		});
 	}
 
+	SignIn() : void {
+		let signIn: SignIn = {profileEmail: this.signInForm.value.profileEmail, profilePassword: this.signInForm.value.profilePassword};
 
-
-	signIn(): void {
-		localStorage.removeItem("jwt-token");
-		this.SignInService.postSignIn(this.signin).subscribe(status => {
+		this.signInService.postSignIn(signIn).subscribe(status => {
 			this.status = status;
-
-
 			if(status.status === 200) {
 
-				this.router.navigate(["profile-page"]);
-				//location.reload(true);
-				this.signInForm.reset();
-				setTimeout(1000,function(){$("#signin-modal").modal('hide');});
 			} else {
-				console.log("failed login")
+				alert("Incorrect Email or Password. Try Again.")
 			}
-		});
+		})
 	}
 }
