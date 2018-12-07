e911b176132a9848d323d97b34878572d208381c
dsanderson90 <dsanderson3@cnm.edu>
2018-12-07 08:28:47 -0700

    8182 - switched signup call to match with DB

---
+++ src/app/foodtruck/foodtruck.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'foodtruck',
+  templateUrl: './foodtruck.component.html',
+  styleUrls: ['./foodtruck.component.css']
+})
+export class FoodtruckComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
