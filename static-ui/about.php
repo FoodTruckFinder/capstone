74d123efc8c11790a3d37777a0d8cd7eb2787332
Greg Klein <shiberino@outlook.com>
2018-12-08 05:16:01 -0700

    updating about page
               @asana 0174

--- static-ui/about.php
+++ static-ui/about.php
@@ -1,15 +1,29 @@
+<!--bootstrap CSS-->
+<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-<?php require_once ("head-utils.php");?>
-<?php require_once("navbar.php");?>
+<!--Bootstrap JS-->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
 
+<!--google script-->
+<script src="https://apis.google.com/js/platform.js" async defer></script>
+
+<!--font awesome-->
+<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.2.0/css/all.css" integrity="sha384-hWVjflwFxL6sNzntih27bfxkr27PmbbK/iSvJ+a4+0owXq79v+lsFkW54bOGbiDQ" crossorigin="anonymous">
+
+<!--our custom CSS-->
+<link rel="stylesheet" type="text/css" href="/src/app/app.css">
+
+<navbar></navbar>
 <main>
 	<div class="container text-center pt-3">
 		<h1>What is Food Truck Finder?</h1>
 
 			<!--toDo list people from left to right according to team photo-->
-			<p>Food Truck Finder was built by (toDo list people from left to right according to team photo)</p>
+			<p>Food Truck Finder was built by (todo)</p>
 
-			<p>Food Truck Finder is a web app designed to help food truck users easily find the best food nearby as well as a resource for food truck owners to let their fans know when they are open for business, what items are on the menu, and more. Albuquerque has great food, and we've created an app that helps both locals and tourists find the best food as quickly as possible.</p>
+			<p>Food Truck Finder is a web app designed to help food truck users easily find the best food nearby as well as a resource for food truck owners to let their fans know when they are open for business, what items are on the menu, and more. Albuquerque is known for its local cuisine, and we've created an app that helps both locals and tourists find the best food as quickly as possible.</p>
 
 			<h2>The Food Truck 5 Team</h2>
 
