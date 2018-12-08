89d5dee882d27077cd9326f82270a8ea0846b8a8
Greg Klein <shiberino@outlook.com>
2018-12-08 06:38:28 -0700

    updating about page with pics!
               @asana 0174

--- static-ui/about.php
+++ static-ui/about.php
@@ -30,9 +30,45 @@
 			<!--toDo add image of team-->
 			<img class="teamphoto" src="https://www.dropbox.com/s/qs9g8hm868j3ooa/teamphoto.png?raw=1" alt="image of the members of the food truck finder team">
 			<h2>Check out the Food Truck Finder GitHub Repo</h2>
-			<a href="https://github.com/FoodTruckFinder"><i class="fab fa-github"></i></a>
+			<a href="https://github.com/FoodTruckFinder"><i class="fab fa-github giticon"></i></a>
 			<h2>Contact Info</h2>
 			<!--toDo list member contact info as cards in order of appearance on the image--one row on desktop, column on mobile. Card will contain headshot, github profile link (as icon), link to PWP, link to social media channels-->
+			<div class="card mb-3">
+				<img class="card-img-top" src="/src/img/bGray.png" alt="Bernina Gray image">
+				<div class="card-body">
+					<h5 class="card-title">Bernina Gray</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				</div>
+			</div>
+			<div class="card mb-3">
+				<img class="card-img-top" src="/src/img/dNakitare.png" alt="Daniel Nakitare">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				</div>
+			</div>
+			<div class="card mb-3">
+				<img class="card-img-top" src="/src/img/dSanderson.png" alt="David Sanderson">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				</div>
+			</div>
+			<div class="card mb-3">
+				<img class="card-img-top" src="/src/img/rJack.png" alt="Rae Jack">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				</div>
+			</div>
+			<div class="card mb-3">
+				<img class="card-img-top" src="/src/img/gKlein.jpg" alt="Greg Klein">
+				<div class="card-body">
+					<h5 class="card-title">Card title</h5>
+					<p class="card-text">This is a wider card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.</p>
+				</div>
+			</div>
+
 		</div>
 	</div>
 </main>
