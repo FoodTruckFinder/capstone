2696c6b2b316e7c7db4547b55088f31bebb4a039
Greg Klein <shiberino@outlook.com>
2018-12-08 11:58:18 -0700

    updating app.module.ts to work with ngui map     @asana 0174

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,15 +1,24 @@
 import {NgModule} from "@angular/core";
-import {HttpClientModule} from "@angular/common/http";
 import {BrowserModule} from "@angular/platform-browser";
+import {HttpClientModule} from "@angular/common/http";
+
 import {AppComponent} from "./app.component";
 import {allAppComponents, appRoutingProviders, routing} from "./app.routes";
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NguiMapModule } from '@ngui/map';
 import {ReactiveFormsModule} from "@angular/forms";
 
 const moduleDeclarations = [AppComponent];
 
 @NgModule({
-	imports:      [BrowserModule, HttpClientModule, routing, NgbModule, ReactiveFormsModule],
+	imports:      [
+		BrowserModule,
+		HttpClientModule,
+		routing,
+		NgbModule,
+		ReactiveFormsModule,
+		NguiMapModule.forRoot({apiUrl: 'https://maps.google.com/maps/api/js?key=AIzaSyCzkPjQz_zGaHXvnmEYO5u_g8LsKP7IxTA'}
+	],
 	declarations: [...moduleDeclarations, ...allAppComponents],
 	bootstrap:    [AppComponent],
 	providers:    [...appRoutingProviders],
