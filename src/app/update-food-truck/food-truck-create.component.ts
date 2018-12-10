78d1e4667d2245ddf5e4c95b0a8b9833e4099a87
Greg Klein <shiberino@outlook.com>
2018-12-10 14:34:16 -0700

    fixing the code being commented out

--- src/app/update-food-truck/food-truck-create.component.ts
+++ src/app/update-food-truck/food-truck-create.component.ts
@@ -9,34 +9,38 @@ import {Observable} from 'rxjs';
 import 'rxjs/add/observable/from';
 import {ActivatedRoute, Router} from "@angular/router";
 
+
 @Component({
 	selector: "food-truck-create",
 	template: require("./food-truck-create.template.html")
 })
 
 //took out on the onInit because Lost Paws only uses it in the edit ts
-export class FoodTruckCreateComponent  {/*
+export class FoodTruckCreateComponent  {
 	foodTruckForm: FormGroup;
 	submitted: boolean = false;
-	status: Status = {status: null, message: null, type: null};
+	status: Status = null;
+	foodtruck: FoodTruck;
 
 	foodTruckId = this.route.snapshot.params["foodTruckId"];
 	success: boolean = false;
 	imageUploaded: boolean = false;
 
+
 	public uploader: FileUploader = new FileUploader({
-		//todo not sure what itemAlias is or how it should be named
-		itemAlias: 'foodtruck',
-		url: './api/image/',
-		headers: [
-			{name: 'X-JWT-TOKEN', value: window.localStorage.getItem('jwt-token')},
-			{name: 'X-XSRF-TOKEN', value: Cookie.get('XSRF-TOKEN')}
-		],
-		additionalParameter: {}
-	});
+			//todo not sure what itemAlias is or how it should be named
+			itemAlias: 'foodtruck',
+			url: './api/image/',
+			headers: [
+				{name: 'X-JWT-TOKEN', value: window.localStorage.getItem('jwt-token')},
+				{name: 'X-XSRF-TOKEN', value: Cookie.get('XSRF-TOKEN')}
+			],
+			additionalParameter: {}
+		}
+	);
+
 
 	cloudinarySecureUrl: string;
-	//cloudinarySecureUrl2: string;
 	cloudinaryPublicObservable: Observable<string> = new Observable<string>();
 
 
@@ -50,52 +54,48 @@ export class FoodTruckCreateComponent  {/*
 			foodTruckPhoneNumber: ["", [Validators.maxLength(16), Validators.required]],
 			foodTruckMenuUrl: ["", [Validators.maxLength(255), Validators.required]],
 		});
-}
+	}
 
-uploadImage(): void {
-	this.uploader.uploadAll();
-	this.cloudinaryPublicObservable.subscribe(cloudinarySecureUrl => this.cloudinarySecureUrl = cloudinarySecureUrl);
-	this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any) => {
-		let reply = JSON.parse(response);
-		this.cloudinarySecureUrl = reply.data;
-		this.cloudinaryPublicObservable = Observable.from(this.cloudinarySecureUrl);
-		if(this.cloudinarySecureUrl) {
-			this.imageUploaded = true;
-		}
-	};
-}
-
-createFoodTruck(): FoodTruck {
-	if(this.cloudinarySecureUrl) {
-		return {
-			foodTruckId: null,
-			foodTruckProfileId: null,
-			foodTruckDescription: this.foodTruckForm.value.foodTruckDescription,
-			foodTruckImageUrl: this.cloudinarySecureUrl,
-			foodTruckPhoneNumber: this.foodTruckForm.value.foodTruckPhoneNumber,
-			foodTruckName: this.foodTruckForm.value.foodTruckName,
-			foodTruckMenuUrl: this.foodTruckForm.value.foodTruckMenuUrl
+	uploadImage(): void {
+		this.uploader.uploadAll();
+		this.cloudinaryPublicObservable.subscribe(cloudinarySecureUrl => this.cloudinarySecureUrl = cloudinarySecureUrl);
+		this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any) => {
+			let reply = JSON.parse(response);
+			this.cloudinarySecureUrl = reply.data;
+			this.cloudinaryPublicObservable = Observable.from(this.cloudinarySecureUrl);
+			if(this.cloudinarySecureUrl) {
+				this.imageUploaded = true;
+			}
 		};
-
-
 	}
-}
 
-postFoodTruck(): void {
-	if(this.cloudinarySecureUrl) {
-		this.submitted = true;
-
-		this.createFoodTruck();
-		let foodTruck : FoodTruck = this.createFoodTruck();
-		if(foodTruck) {
-			this.foodTruckService.createFoodTruck(foodTruck).subscribe(status => {
-				this.status = status;
-				if(this.status.status === 200) {
-					this.foodTruckForm.reset();
-				}
-			});
+	createFoodTruck(): FoodTruck {
+		if(this.cloudinarySecureUrl) {
+			return {
+				foodTruckId: null,
+				foodTruckProfileId: null,
+				foodTruckDescription: this.foodTruckForm.value.foodTruckDescription,
+				foodTruckImageUrl: this.cloudinarySecureUrl,
+				foodTruckPhoneNumber: this.foodTruckForm.value.foodTruckPhoneNumber,
+				foodTruckName: this.foodTruckForm.value.foodTruckName,
+				foodTruckMenuUrl: this.foodTruckForm.value.foodTruckMenuUrl
+			};
 		}
 	}
-}
-*/
+
+	postFoodTruck(): void {
+		if(this.cloudinarySecureUrl) {
+			this.submitted = true;
+			this.createFoodTruck();
+			let foodTruck: FoodTruck = this.createFoodTruck();
+			if(foodTruck) {
+				this.foodTruckService.createFoodTruck(foodTruck).subscribe(status => {
+					this.status = status;
+					if(this.status.status === 200) {
+						this.foodTruckForm.reset();
+					}
+				});
+			}
+		}
+	}
 }
