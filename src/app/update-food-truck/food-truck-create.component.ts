fdded3b36f3ff8ddb7e503459cf0a947f9225d72
Brittany jack <jackbrittany54@yahoo.com>
2018-12-11 14:57:57 -0700

    added pt-5 on card deck on food truck card page 0171

--- src/app/update-food-truck/food-truck-create.component.ts
+++ src/app/update-food-truck/food-truck-create.component.ts
@@ -1,103 +1,103 @@
-
-
-import {Component, OnInit} from "@angular/core";
-import {FormBuilder, FormGroup, Validators} from "@angular/forms";
-import {FoodTruck} from "../shared/interfaces/foodtruck";
-import {FoodTruckService} from "../shared/services/foodtruck.service";
-import {Status} from "../shared/interfaces/status";
-import {FileUploader} from 'ng2-file-upload';
-import {Cookie} from 'ng2-cookies';
-import {Observable} from 'rxjs';
-import 'rxjs/add/observable/from';
-import {ActivatedRoute, Router} from "@angular/router";
-
-
-@Component({
-	selector: "food-truck-create",
-	template: require("./food-truck-create.template.html")
-})
-
-//took out on the onInit because Lost Paws only uses it in the edit ts
-export class FoodTruckCreateComponent  {
-	foodTruckForm: FormGroup;
-	submitted: boolean = false;
-	status: Status = null;
-	foodtruck: FoodTruck;
-
-	foodTruckId = this.route.snapshot.params["foodTruckId"];
-	success: boolean = false;
-	imageUploaded: boolean = false;
-
-
-	public uploader: FileUploader = new FileUploader({
-			//todo not sure what itemAlias is or how it should be named
-			itemAlias: 'foodtruck',
-			url: './api/image/',
-			headers: [
-				{name: 'X-JWT-TOKEN', value: window.localStorage.getItem('jwt-token')},
-				{name: 'X-XSRF-TOKEN', value: Cookie.get('XSRF-TOKEN')}
-			],
-			additionalParameter: {}
-		}
-	);
-
-
-	cloudinarySecureUrl: string;
-	cloudinaryPublicObservable: Observable<string> = new Observable<string>();
-
-
-	constructor(protected foodTruckService: FoodTruckService,
-					protected formBuilder: FormBuilder,
-					protected route: ActivatedRoute,
-					protected router: Router) {
-		this.foodTruckForm = this.formBuilder.group({
-			foodTruckName: ["", [Validators.maxLength(128), Validators.required]],
-			foodTruckDescription: ["", [Validators.maxLength(256), Validators.required]],
-			foodTruckPhoneNumber: ["", [Validators.maxLength(16), Validators.required]],
-			foodTruckMenuUrl: ["", [Validators.maxLength(255), Validators.required]],
-		});
-	}
-
-	uploadImage(): void {
-		this.uploader.uploadAll();
-		this.cloudinaryPublicObservable.subscribe(cloudinarySecureUrl => this.cloudinarySecureUrl = cloudinarySecureUrl);
-		this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any) => {
-			let reply = JSON.parse(response);
-			this.cloudinarySecureUrl = reply.data;
-			this.cloudinaryPublicObservable = Observable.from(this.cloudinarySecureUrl);
-			if(this.cloudinarySecureUrl) {
-				this.imageUploaded = true;
-			}
-		};
-	}
-
-	createFoodTruck(): FoodTruck {
-		if(this.cloudinarySecureUrl) {
-			return {
-				foodTruckId: null,
-				foodTruckProfileId: null,
-				foodTruckDescription: this.foodTruckForm.value.foodTruckDescription,
-				foodTruckImageUrl: this.cloudinarySecureUrl,
-				foodTruckPhoneNumber: this.foodTruckForm.value.foodTruckPhoneNumber,
-				foodTruckName: this.foodTruckForm.value.foodTruckName,
-				foodTruckMenuUrl: this.foodTruckForm.value.foodTruckMenuUrl
-			};
-		}
-	}
-
-	postFoodTruck(): void {
-		if(this.cloudinarySecureUrl) {
-			this.submitted = true;
-			this.createFoodTruck();
-			let foodTruck: FoodTruck = this.createFoodTruck();
-			if(foodTruck) {
-				this.foodTruckService.createFoodTruck(foodTruck).subscribe(status => {
-					this.status = status;
-					if(this.status.status === 200) {
-						this.foodTruckForm.reset();
-					}
-				});
-			}
-		}
-	}
-}
+//
+//
+// import {Component, OnInit} from "@angular/core";
+// import {FormBuilder, FormGroup, Validators} from "@angular/forms";
+// import {FoodTruck} from "../shared/interfaces/foodtruck";
+// import {FoodTruckService} from "../shared/services/foodtruck.service";
+// import {Status} from "../shared/interfaces/status";
+// import {FileUploader} from 'ng2-file-upload';
+// import {Cookie} from 'ng2-cookies';
+// import {Observable} from 'rxjs';
+// import 'rxjs/add/observable/from';
+// import {ActivatedRoute, Router} from "@angular/router";
+//
+//
+// @Component({
+// 	selector: "food-truck-create",
+// 	template: require("./food-truck-create.template.html")
+// })
+//
+// //took out on the onInit because Lost Paws only uses it in the edit ts
+// export class FoodTruckCreateComponent  {
+// 	foodTruckForm: FormGroup;
+// 	submitted: boolean = false;
+// 	status: Status = null;
+// 	foodtruck: FoodTruck;
+//
+// 	foodTruckId = this.route.snapshot.params["foodTruckId"];
+// 	success: boolean = false;
+// 	imageUploaded: boolean = false;
+//
+//
+// 	public uploader: FileUploader = new FileUploader({
+// 			//todo not sure what itemAlias is or how it should be named
+// 			itemAlias: 'foodtruck',
+// 			url: './api/image/',
+// 			headers: [
+// 				{name: 'X-JWT-TOKEN', value: window.localStorage.getItem('jwt-token')},
+// 				{name: 'X-XSRF-TOKEN', value: Cookie.get('XSRF-TOKEN')}
+// 			],
+// 			additionalParameter: {}
+// 		}
+// 	);
+//
+//
+// 	cloudinarySecureUrl: string;
+// 	cloudinaryPublicObservable: Observable<string> = new Observable<string>();
+//
+//
+// 	constructor(protected foodTruckService: FoodTruckService,
+// 					protected formBuilder: FormBuilder,
+// 					protected route: ActivatedRoute,
+// 					protected router: Router) {
+// 		this.foodTruckForm = this.formBuilder.group({
+// 			foodTruckName: ["", [Validators.maxLength(128), Validators.required]],
+// 			foodTruckDescription: ["", [Validators.maxLength(256), Validators.required]],
+// 			foodTruckPhoneNumber: ["", [Validators.maxLength(16), Validators.required]],
+// 			foodTruckMenuUrl: ["", [Validators.maxLength(255), Validators.required]],
+// 		});
+// 	}
+//
+// 	uploadImage(): void {
+// 		this.uploader.uploadAll();
+// 		this.cloudinaryPublicObservable.subscribe(cloudinarySecureUrl => this.cloudinarySecureUrl = cloudinarySecureUrl);
+// 		this.uploader.onSuccessItem = (item: any, response: string, status: number, headers: any) => {
+// 			let reply = JSON.parse(response);
+// 			this.cloudinarySecureUrl = reply.data;
+// 			this.cloudinaryPublicObservable = Observable.from(this.cloudinarySecureUrl);
+// 			if(this.cloudinarySecureUrl) {
+// 				this.imageUploaded = true;
+// 			}
+// 		};
+// 	}
+//
+// 	createFoodTruck(): FoodTruck {
+// 		if(this.cloudinarySecureUrl) {
+// 			return {
+// 				foodTruckId: null,
+// 				foodTruckProfileId: null,
+// 				foodTruckDescription: this.foodTruckForm.value.foodTruckDescription,
+// 				foodTruckImageUrl: this.cloudinarySecureUrl,
+// 				foodTruckPhoneNumber: this.foodTruckForm.value.foodTruckPhoneNumber,
+// 				foodTruckName: this.foodTruckForm.value.foodTruckName,
+// 				foodTruckMenuUrl: this.foodTruckForm.value.foodTruckMenuUrl
+// 			};
+// 		}
+// 	}
+//
+// 	postFoodTruck(): void {
+// 		if(this.cloudinarySecureUrl) {
+// 			this.submitted = true;
+// 			this.createFoodTruck();
+// 			let foodTruck: FoodTruck = this.createFoodTruck();
+// 			if(foodTruck) {
+// 				this.foodTruckService.createFoodTruck(foodTruck).subscribe(status => {
+// 					this.status = status;
+// 					if(this.status.status === 200) {
+// 						this.foodTruckForm.reset();
+// 					}
+// 				});
+// 			}
+// 		}
+// 	}
+// }
--- src/app/update-food-truck/food-truck-edit.component.ts
+++ src/app/update-food-truck/food-truck-edit.component.ts
@@ -1,67 +1,67 @@
-import {Component, OnInit} from "@angular/core";
-import {FormBuilder, FormGroup, Validators} from '@angular/forms';
-import {FoodTruck} from "../shared/interfaces/foodtruck";
-import {FoodTruckService} from "../shared/services/foodtruck.service";
-import {Status} from "../shared/interfaces/status";
-import 'rxjs/add/observable/from';
-import {ActivatedRoute, Router} from "@angular/router";
-
-@Component({
-	template: require("./food-truck-edit.template.html"),
-	selector: "food-truck-edit"
-})
-
-export class UpdateFoodTruckComponent implements OnInit {
-	foodTruckForm: FormGroup;
-	submitted: boolean = false;
-	status: Status = null;
-	foodTruck: FoodTruck = {foodTruckId: null, foodTruckProfileId: null, foodTruckDescription: null, foodTruckImageUrl: null, foodTruckMenuUrl: null, foodTruckName: null, foodTruckPhoneNumber: null};
-	foodTruckId = this.route.snapshot.params["foodTruckId"];
-	success: boolean = false;
-
-	constructor(protected foodTruckService: FoodTruckService,
-					protected formBuilder: FormBuilder,
-					protected route: ActivatedRoute,
-					protected router: Router) {
-		this.foodTruckForm = this.formBuilder.group({
-			foodTruckName: ["", [Validators.maxLength(128), Validators.required]],
-			foodTruckDescription: ["", [Validators.maxLength(256), Validators.required]],
-			foodTruckPhoneNumber: ["", [Validators.maxLength(16), Validators.required]]
-			//commenting these out because we don't need image editing for our prototype
-			//foodTruckImageUrl: ["", [Validators.maxLength(255), Validators.required]],
-			//foodTruckMenuUrl: ["", [Validators.maxLength(255), Validators.required]]
-		});
-	}
-
-	ngOnInit(){
-		this.applyFormChanges();
-		this.loadFoodTruckValues();
-	}
-
-	applyFormChanges(): void {
-		this.foodTruckForm.valueChanges.subscribe(values => {
-			for(let field in values) {
-				this.foodTruck[field] = values[field];
-			}
-		});
-	}
-
-	loadFoodTruckValues() {
-		this.foodTruckService.getFoodTruckByFoodTruckId(this.foodTruckId).subscribe(foodTruck => {
-			this.foodTruck = foodTruck;
-			this.foodTruckForm.patchValue(foodTruck);
-		});
-	}
-
-	editFoodTruck() {
-		this.foodTruckService.editFoodTruck(this.foodTruck).subscribe(status => {
-			this.status = status;
-		});
-		this.foodTruckForm.reset();
-		if(this.status.status === 200) {
-			this.success = true;
-			// this.router.navigate(["foodTruck/:foodTruckId"]);
-		}
-	}
-}
-
+// import {Component, OnInit} from "@angular/core";
+// import {FormBuilder, FormGroup, Validators} from '@angular/forms';
+// import {FoodTruck} from "../shared/interfaces/foodtruck";
+// import {FoodTruckService} from "../shared/services/foodtruck.service";
+// import {Status} from "../shared/interfaces/status";
+// import 'rxjs/add/observable/from';
+// import {ActivatedRoute, Router} from "@angular/router";
+//
+// @Component({
+// 	template: require("./food-truck-edit.template.html"),
+// 	selector: "food-truck-edit"
+// })
+//
+// export class UpdateFoodTruckComponent implements OnInit {
+// 	foodTruckForm: FormGroup;
+// 	submitted: boolean = false;
+// 	status: Status = null;
+// 	foodTruck: FoodTruck = {foodTruckId: null, foodTruckProfileId: null, foodTruckDescription: null, foodTruckImageUrl: null, foodTruckMenuUrl: null, foodTruckName: null, foodTruckPhoneNumber: null};
+// 	foodTruckId = this.route.snapshot.params["foodTruckId"];
+// 	success: boolean = false;
+//
+// 	constructor(protected foodTruckService: FoodTruckService,
+// 					protected formBuilder: FormBuilder,
+// 					protected route: ActivatedRoute,
+// 					protected router: Router) {
+// 		this.foodTruckForm = this.formBuilder.group({
+// 			foodTruckName: ["", [Validators.maxLength(128), Validators.required]],
+// 			foodTruckDescription: ["", [Validators.maxLength(256), Validators.required]],
+// 			foodTruckPhoneNumber: ["", [Validators.maxLength(16), Validators.required]]
+// 			//commenting these out because we don't need image editing for our prototype
+// 			//foodTruckImageUrl: ["", [Validators.maxLength(255), Validators.required]],
+// 			//foodTruckMenuUrl: ["", [Validators.maxLength(255), Validators.required]]
+// 		});
+// 	}
+//
+// 	ngOnInit(){
+// 		this.applyFormChanges();
+// 		this.loadFoodTruckValues();
+// 	}
+//
+// 	applyFormChanges(): void {
+// 		this.foodTruckForm.valueChanges.subscribe(values => {
+// 			for(let field in values) {
+// 				this.foodTruck[field] = values[field];
+// 			}
+// 		});
+// 	}
+//
+// 	loadFoodTruckValues() {
+// 		this.foodTruckService.getFoodTruckByFoodTruckId(this.foodTruckId).subscribe(foodTruck => {
+// 			this.foodTruck = foodTruck;
+// 			this.foodTruckForm.patchValue(foodTruck);
+// 		});
+// 	}
+//
+// 	editFoodTruck() {
+// 		this.foodTruckService.editFoodTruck(this.foodTruck).subscribe(status => {
+// 			this.status = status;
+// 		});
+// 		this.foodTruckForm.reset();
+// 		if(this.status.status === 200) {
+// 			this.success = true;
+// 			// this.router.navigate(["foodTruck/:foodTruckId"]);
+// 		}
+// 	}
+// }
+//
