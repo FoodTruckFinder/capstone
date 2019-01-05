822180edeb8cde572f9dcbe1372cff7cea85890d
Daniel Nakitare <dnakitare@cnm.edu>
2019-01-05 11:11:36 -0700

    Changed about.php page and changed "webpack-dev-server": ">=3.1.11" devDependencies

--- static-ui/about.php
+++ static-ui/about.php
@@ -43,7 +43,7 @@
 				<img class="card-img-top rounded-circle pt-4" src="images/dNakitare.jpg" alt="Daniel Nakitare">
 				<div class="card-body">
 					<h3 class="card-title">Daniel Nakitare</h3>
-					<p class="card-text">Originally from Maine, I moved to Rio Rancho after retiring from the Army and have fallen in love with the South West. After getting an MBA, I was still searching for a fulfilling career and stumbled across the Deep Dive Bootcamp. Web development allows me to build things, solve problems, and connect people and the world. I am hooked and have never looked back.</p>
+					<p class="card-text">Originally from Maine, I moved to Rio Rancho after retiring from the Army and have fallen in love with the South West. After getting an MBA, I was still searching for a fulfilling career and stumbled across the Deep Dive Bootcamp. Web development allows me to build things, solve problems, and connect people. Learn and build.</p>
 					<a href="http://nakitare.io" class="btn btn-warning">Visit My Website</a>
 				</div>
 			</div>
