ae26c6d62b127c437be0c5e8b02a2c10dc56ae28
David J. Sanderson <sanderdj90@gmail.com>
2020-06-06 16:02:56 -0600

    fix: update syntax

--- models/user.tsx
+++ models/user.tsx
@@ -3,7 +3,7 @@ export interface User {
     name: string;
     email: string;
     password: string;
-    isOwner: boolean,
-    foodtruckName: string | null
+    isOwner: boolean;
+    foodtruckName?: string;
   }
   
